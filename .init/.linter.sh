93473b1924f5722d620a663b01d10b5e6294748b
Kavia AI <production@kavia.ai>
2026-02-05 07:41:04 +0000

    Checkpoint 1 - cge3ff4136

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/flutter-authentication-demo-9808-9817/auth_demo_frontend
+flutter analyze
+
---
+++ auth_demo_frontend/android/app/src/main/kotlin/com/example/auth_demo_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.auth_demo_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
