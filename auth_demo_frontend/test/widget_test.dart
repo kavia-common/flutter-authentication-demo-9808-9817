655ee0875d26d8eafe744de0e7771f7b5c730176
Kavia AI <production@kavia.ai>
2026-02-05 10:10:58 +0000

    Checkpoint 5 - cge3ff4136

--- auth_demo_frontend/test/widget_test.dart
+++ auth_demo_frontend/test/widget_test.dart
@@ -1,15 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:auth_demo_frontend/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Shows Log In text', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: Text('Log In'),
-        ),
-      ),
-    );
+    await tester.pumpWidget(const AuthDemoApp());
     await tester.pumpAndSettle();
 
     expect(find.text('Log In'), findsOneWidget);
